15f4d6c634d9bc0044181210803b3381bf2c8c56
Manuel0217-m <emmanuel.arrubla771@pascualbravo.edu.co>
2025-03-07 13:50:57 -0500

    Add files via upload

---
+++ Biblioteca.Resta/Biblioteca.Resta/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Resta
+{
+    public class OperacionResta
+    {
+        
+        public int Restar(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+    }
+}
