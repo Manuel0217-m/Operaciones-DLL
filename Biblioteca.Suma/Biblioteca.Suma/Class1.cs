0a85385362710b712a18e339f02fbc3c1b43699e
Manuel0217-m <emmanuel.arrubla771@pascualbravo.edu.co>
2025-03-07 13:44:18 -0500

    Suma

---
+++ Biblioteca.Suma/Biblioteca.Suma/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Suma
+{
+    public class OperacionSuma
+    {
+       
+        public int Sumar(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
